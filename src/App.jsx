c66b4bb8b06a94f8dc2180c0eb8bb64e8343e4a4
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-09-23 21:14:35 +0100

    sething the route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import Homepage from "./pages/Homepage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CoinDetails from "./pages/CoinDetails";
 
 function App() {
   return (
     <>
-      <Homepage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/:coin" element={<CoinDetails />} />
+        </Routes>
+      </Router>
     </>
   );
 }
