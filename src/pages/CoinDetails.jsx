120e02f9f55c605cecfd461343e4fc79e3c9f771
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-09-25 22:20:02 +0100

    oh

--- src/pages/CoinDetails.jsx
+++ src/pages/CoinDetails.jsx
@@ -1,17 +1,8 @@
-import {
-  Spinner,
-  Table,
-  TableContainer,
-  Tbody,
-  Text,
-  Th,
-  Thead,
-  Tr,
-} from "@chakra-ui/react";
 import axios from "axios";
 import React, { useEffect } from "react";
 import { useState } from "react";
 import millify from "millify";
+import "../App.css";
 
 const CoinDetails = () => {
   const [coin, setCoin] = useState({});
@@ -28,7 +19,7 @@ const CoinDetails = () => {
 
         setTimeout(() => {
           setLoading(false);
-        }, 1500);
+        }, 1000);
       })
       .catch((error) => {
         console.log(error);
@@ -36,28 +27,23 @@ const CoinDetails = () => {
   }, []);
 
   return (
-    <>
+    <div className="details">
       {loading ? (
-        <Spinner
-          thickness="4px"
-          speed="0.65s"
-          emptyColor="gray.200"
-          color="blue.500"
-          size="xl"
-        />
+        <p>loading...</p>
       ) : (
-        <>
-          <Text> Market Cap Rank:{coin.market_cap_rank}</Text>
-          <Text>{coin.name}</Text>
-          <Text>Score : {coin.coingecko_score}</Text>
-          <Text>Price:{coin.market_data.current_price.usd}</Text>
-          <Text>24hr high:{coin.market_data.high_24h.usd}</Text>
-          <Text>24hr low:{coin.market_data.low_24h.usd}</Text>
-          <Text>Mkt:{coin.market_data.market_cap.usd}</Text>
-          <Text>Description:{coin.description.en}</Text>
-        </>
+        <div>
+          <p> Market Cap Rank:{coin.market_cap_rank}</p>
+          <p>{coin.name}</p>
+          <p>Score : {coin.coingecko_score}</p>
+          <p>Price:{coin.market_data.current_price.usd}</p>
+          <p>24hr high:{coin.market_data.high_24h.usd}</p>
+          {millify(d.high_24h)}
+          <p>24hr low:{coin.market_data.low_24h.usd}</p>
+          <p>Mkt:{coin.market_data.market_cap.usd}</p>
+          <p>Description:{coin.description.en}</p>
+        </div>
       )}
-    </>
+    </div>
   );
 };
 
