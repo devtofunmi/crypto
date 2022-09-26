5be6bc22b9edfc1d47af01850f7bdf2780916d31
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-09-26 23:44:00 +0100

    add some stuff

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -10,6 +10,8 @@ import "../App.css";
 function Homepage() {
   const [data, setData] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [fav, setFav] = useState([]);
+
   const url =
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
 
@@ -18,7 +20,6 @@ function Homepage() {
       .get(url)
       .then((response) => {
         setData(response.data);
-        console.log;
       })
       .catch((error) => {
         console.log(error);
@@ -34,6 +35,28 @@ function Homepage() {
       );
     }
   };
+
+  useEffect(() => {
+    if (!localStorage.getItem("fav")) {
+      localStorage.setItem("fav", JSON.stringify([]));
+    } else {
+      setFav(JSON.parse(localStorage.getItem("fav")));
+    }
+  }, []);
+
+  const addToFav = (coinId) => {
+    if (fav.includes(coinId)) {
+      const newFav = fav.filter((coin) => coin !== coinId);
+      console.log(newFav);
+      setFav(newFav);
+      localStorage.setItem("fav", JSON.stringify(newFav));
+    } else {
+      const newFav = [...fav, coinId];
+      setFav(newFav);
+      localStorage.setItem("fav", JSON.stringify(newFav));
+    }
+  };
+
   return (
     <div className="container">
       <div>
@@ -43,29 +66,50 @@ function Homepage() {
             placeholder="search"
             onChange={(e) => setSearchTerm(e.target.value)}
           />
+          <button>Fav</button>
         </div>
+
         <div className="heading">
           <p>#</p>
           <p>Coin</p>
           <p>Price</p>
-          <p>24hr</p>
-          <p>Mkt Cap</p>
+          <p className="hide">24hr</p>
+          <p className="hide">Mkt Cap</p>
+          <p>24hr %</p>
+          <p>Add to Fav</p>
         </div>
 
         <div>
           {filteredData().map((d) => (
-            <Link to={`/${d.id}`} key={d.market_cap_rank} className="coin-row">
+            <div key={d.market_cap_rank} className="coin-row">
               <p>{d.market_cap_rank}</p>
 
-              <div className="coin-name-and-logo">
+              <Link to={`/${d.id}`} className="coin-name-and-logo">
                 <img src={d.image} />
                 <p>{d.name}</p>
-              </div>
+              </Link>
 
-              <p>$ {millify(d.high_24h)}</p>
+              <p className="hide">$ {millify(d.high_24h)}</p>
               <p>${millify(d.current_price)}</p>
-              <p>${millify(d.market_cap)}</p>
-            </Link>
+              <p className="hide">${millify(d.market_cap)}</p>
+              <p
+                style={{
+                  color:
+                    d.market_cap_change_percentage_24h < 1 ? "red" : "green",
+                }}
+              >
+                {millify(d.market_cap_change_percentage_24h)}
+              </p>
+
+              <button
+                className="btn"
+                onClick={() => {
+                  addToFav(d.id);
+                }}
+              >
+                {fav.includes(d.id) ? "Remove from fav" : "Add to Fav"}
+              </button>
+            </div>
           ))}
         </div>
       </div>
